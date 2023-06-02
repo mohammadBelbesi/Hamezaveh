9cafcf41c8d6873dc261d6250185d88a91e5d357
mohammadBelbesi <my_1999@live.com>
2023-06-02 10:41:52 +0300

    start cartPage and redux store enviroment

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ const router = createBrowserRouter([
         path: "/cart",
         element: <Cart />,
       },
+      
     ],
   },
 ]);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { cart, loginBlack, loginPerson, logo } from "../assets/assetsindex";
 import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const productData = useSelector((state) => state.bazar.productData);
+  // console.log(productData);
   return (
     <div dir="ltr" className="w-full h-36 border-b-[1px] border-b-gray-800 sticky top-0 z-50">
       <div className="max-w-screen-xl h-full mx-auto flex items-center justify-between">
@@ -27,7 +30,7 @@ const Header = () => {
               onMouseOver={(e) => { e.target.style.opacity = 0.8; e.target.style.boxShadow = '0 2px 6px rgba(0, 0, 0, 0.2)'; }}
               onMouseOut={(e) => { e.target.style.opacity = 1; e.target.style.boxShadow = 'none'; }}
             />
-            <span className="zero absolute w-12 h-10 mx-auto top-1 right-1 text-sm flex items-center justify-center font-serif">0</span>
+            <span className="zero absolute w-12 h-10 mx-auto top-1 right-1 text-sm flex items-center justify-center font-serif">{productData.length}</span>
           </div>
           </Link>
           <ul className="flex items-center gap-20 ">
