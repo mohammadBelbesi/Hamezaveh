8440d74528af85db986532e1a011c972e8dc58ee
mohammadBelbesi <my_1999@live.com>
2023-06-07 23:07:15 +0300

    login check for cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { cartCover } from '../assets/assetsindex';
 import Header from "../components/homePage/Header";
 import Footer from "../components/homePage/Footer";
@@ -13,6 +13,8 @@ const Cart = () => {
   const [isMember, setIsMember] = useState(false);
   const isCartEmpty = productData.length === 0;
   let member = useSelector((state) => state.bazar.isMember);
+  const isLogin = useSelector((state) => state.bazar.isLogin); // Access isLogin from the bazar slice
+  const dispatch = useDispatch();
 
   useEffect(() => {
     let totalPrice = 0;
@@ -111,58 +113,65 @@ const Cart = () => {
   return (
     <>
       <Header />
-      <div>
-        <img className='w-screen h-cartPage object-cover' src={cartCover} alt='cartCover' />
-        <div className='max-w-screen-xl mx-auto py-20 flex'>
-          {!isCartEmpty ? (
-            <>
-              <CartItem />
-              <div className='w-1/3 bg-[#fafafa] py-6 px-4'>
-                <div className='flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6'>
-                  <h2 className='text-2xl font-medium'>סכום העגלה</h2>
-                  <p className='flex items-center gap-4 text-base'>
-                    סכום{' '}
-                    <span className='font-titleFont font-bold text-lg'>
-                      {totalAmt} ₪
+      {isLogin ? (
+        <div>
+          <img className='w-screen h-cartPage object-cover' src={cartCover} alt='cartCover' />
+          <div className='max-w-screen-xl mx-auto py-20 flex'>
+            {!isCartEmpty ? (
+              <>
+                <CartItem />
+                <div className='w-1/3 bg-[#fafafa] py-6 px-4'>
+                  <div className='flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6'>
+                    <h2 className='text-2xl font-medium'>סכום העגלה</h2>
+                    <p className='flex items-center gap-4 text-base'>
+                      סכום{' '}
+                      <span className='font-titleFont font-bold text-lg'>
+                        {totalAmt} ₪
+                      </span>
+                    </p>
+                  </div>
+                  <p className='font-titleFont font-semibold flex justify-between mt-6'>
+                    הסכום הסופי{' '}
+                    <span className='text-xl font-bold'>
+                      {isMember ? (totalAmt - totalAmt * 0.3).toFixed(2) : totalAmt.toFixed(2)} ₪
                     </span>
                   </p>
+                  <button onClick={() => pay(totalAmt)} className='text-base bg-red-500 text-white w-full py-3 mt-6 hover:bg-red-800 duration-300'>
+                    עבור לתשלום
+                  </button>
                 </div>
-                <p className='font-titleFont font-semibold flex justify-between mt-6'>
-                  הסכום הסופי{' '}
-                  <span className='text-xl font-bold'>
-                    {isMember ? (totalAmt - totalAmt * 0.3).toFixed(2) : totalAmt.toFixed(2)} ₪
-                  </span>
-                </p>
-                <button onClick={() => pay(totalAmt)} className='text-base bg-red-500 text-white w-full py-3 mt-6 hover:bg-red-800 duration-300'>
-                  עבור לתשלום
-                </button>
-              </div>
-            </>
-          ) : (
-            <>
-              <div className='flex flex-col items-center mx-auto'>
-                <div className='w-full text-center'>
-                  <p className='text-3xl mt-4 font-medium text-red-900'>העגלה שלך ריקה</p>
-                  <p className='text-3xl mt-4 font-medium text-red-900'>ניתן להוסיף מוצרים מהחנות לעגלה ולהמשיך לתהליך הרכישה.</p>
+              </>
+            ) : (
+              <>
+                <div className='flex flex-col items-center mx-auto'>
+                  <div className='w-full text-center'>
+                    <p className='text-3xl mt-4 font-medium text-red-900'>העגלה שלך ריקה</p>
+                    <p className='text-3xl mt-4 font-medium text-red-900'>ניתן להוסיף מוצרים מהחנות לעגלה ולהמשיך לתהליך הרכישה.</p>
+                  </div>
+                  <div>
+                    <Link to="/shop">
+                      <button className="mt-8 ml-7 flex items-center gap-1 text-black hover:bg-red-500 duration-300 text-2xl">
+                        <span>
+                          <HiOutlineArrowRight />
+                        </span>
+                        חזרה לחנות
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-                <div>
-                  <Link to="/shop">
-                    <button className="mt-8 ml-7 flex items-center gap-1 text-black hover:bg-red-500 duration-300 text-2xl">
-                      <span>
-                        <HiOutlineArrowRight />
-                      </span>
-                      חזרה לחנות
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </>
-          )}
+              </>
+            )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className='flex flex-col items-center mx-auto'>
+          <p className='text-3xl mt-4 font-medium text-red-900'>אתה צריך להיכנס לחשבונך</p>
+        </div>
+      )}
       <Footer />
     </>
   );
+  
 };
 
 export default Cart;
