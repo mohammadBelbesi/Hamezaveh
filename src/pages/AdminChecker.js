9b16f0ab19b4999020f5a37e515636d44b9c729d
mohammadBelbesi <my_1999@live.com>
2023-06-17 22:33:48 +0300

    css for admin routes

--- src/pages/AdminChecker.js
+++ src/pages/AdminChecker.js
@@ -5,5 +5,10 @@ import Admin from "../admin/Admin";
 export default function AdminChecker() {
   const isAdmin = useSelector((state) => state.bazar.isAdmin);
 
-  return isAdmin ? <Admin /> : <h1>you are not admin!</h1>;
-}
+  return isAdmin ? (
+    <Admin />
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl text-red-500">!You are not an admin</h1>
+    </div>
+  );}
